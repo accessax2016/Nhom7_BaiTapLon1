bfed5eaa0b152db20e31ab7f00fb34f94ba97453
Nguyễn Hoàng Thanh Tùng <nguyenhoangthanhtung1610@gmail.com>
2017-03-30 23:42:43 +0700

    Bai tap lon

---
+++ Nhom7_BaiTapLon/src/main/java/com/hellokoding/springboot/WebApplication.java
@@ -0,0 +1,52 @@
+package com.hellokoding.springboot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.boot.context.web.ErrorPageFilter;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@SpringBootApplication
+public class WebApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebApplication.class);
+        
+    }
+    @Value("${spring.view.prefix}")
+    private String prefix = "";
+    @Value("${spring.view.suffix}")
+    private String suffix = "";
+    @Value("${spring.view.view-names}")
+    private String viewNames = "";
+    @Bean
+    InternalResourceViewResolver jspViewResolver() {
+        final InternalResourceViewResolver vr = new InternalResourceViewResolver();
+        vr.setPrefix(prefix);
+        vr.setSuffix(suffix);
+        vr.setViewClass(JstlView.class);
+        vr.setViewNames(viewNames);
+        return vr;
+    }
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(WebApplication.class, args);
+    }
+    @Bean
+    public ErrorPageFilter errorPageFilter() {
+        return new ErrorPageFilter();
+    }
+
+    @Bean
+    public FilterRegistrationBean disableSpringBootErrorFilter(ErrorPageFilter filter) {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(filter);
+        filterRegistrationBean.setEnabled(false);
+        return filterRegistrationBean;
+    }
+}
+
